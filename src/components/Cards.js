62796d75e2f73dc76c06f7cb34ddbae020c97807
Akash02032002 <aksahkumar02032002@gmail.com>
2024-01-04 06:03:15 +0530

    third commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
  const Cards=()=> {
     return (
         <div>
-            
+            <h1>Anakash</h1>
         </div>
     )
  }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,18 +1,13 @@
 import React from "react";
 
- const Filter=(props)=> {
-    let filterData=props.filterData;
-    return (
-        
-        
-        <div>
-            {
-                filterData.map((data)=>(
-                    <button>{data.title}</button>
-                ))
-            }
-            
-        </div>
-    )
- }
- export default Filter;
+const Filter = (props) => {
+  let filterData = props.filterData;
+  return (
+    <div>
+      {filterData.map((data) => (
+        <button>{data.title}</button>
+      ))}
+    </div>
+  );
+};
+export default Filter;
